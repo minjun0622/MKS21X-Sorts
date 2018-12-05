daa5c08c6981a8c1efd0ffd6877f80efcac32d35
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-12-04 22:12:20 -0500

    I tried to think of using add instead of replacing the value.

--- Sort.java
+++ Sort.java
@@ -5,10 +5,12 @@ public static void selectionsort(int [] ary) {
   }
   for (int i = 0; i < ary.length; i++) {
     if ( (Math.min(ary[i], ary[i + 1])) = ary[i]) {
-      ary[i] = ary[0];
+      add(0, ary[i]);
+      //if the value at i is less than the element next to it, move it to the beginning.
     }
     if ( (Math.max(ary[i], ary[i - 1])) = ary[i]) {
-      ary[i] = ary[ary.length - 1];
+      add(ary.length - 1, ary[i])
+      //if it's greater than move it to the end.
     }
   }
 }
