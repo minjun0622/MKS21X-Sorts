e742080f29f4f78d528381d66cc053c6621f92ff
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-12-09 20:53:34 -0500

    Added the driver. The code passes the test.

--- Sorts.java
+++ Sorts.java
@@ -50,8 +50,9 @@ public static void main(String[] args) {
     int[] test_a = { 3, 4, 7, 1, 6, 2, 8, 6 };
     System.out.println(printArray(test_a));               // [3, 4, 7, 1, 6, 2, 8, 6]
 
-    selectionsort(test_a);
-
+    //selectionsort(test_a);
+    bubbleSort(test_a);
+    
     sorted = printArray(test_a);
     System.out.println(sorted);               // [1, 2, 3, 4, 6, 6, 7, 8]
 
@@ -65,7 +66,8 @@ public static void main(String[] args) {
     int[] test_b = { 17, 1, 19, 2, 18, 20, 1, 18, 11, 13, 5, 17 };
     System.out.println(printArray(test_b));   // [17, 1, 19, 2, 18, 20, 1, 18, 11, 13, 5, 17]
 
-    selectionsort(test_b);
+    //selectionsort(test_b);
+    bubbleSort(test_b);
 
     sorted = printArray(test_b);
     System.out.println(sorted);               // [1, 1, 2, 5, 11, 13, 17, 17, 18, 18, 19, 20]
@@ -80,7 +82,8 @@ public static void main(String[] args) {
     int[] test_c = { 14, 32, 19, 66, 61, 96, 33, 48, 30, 100, 65, 37, 15, 85, 1, 35, 9, 57, 50, 52 };
     System.out.println(printArray(test_c));   // [14, 32, 19, 66, 61, 96, 33, 48, 30, 100, 65, 37, 15, 85, 1, 35, 9, 57, 50, 52]
 
-    selectionsort(test_c);
+    //selectionSort(test_c);
+    bubbleSort(test_c);
 
     sorted = printArray(test_c);
     System.out.println(sorted);               // [1, 9, 14, 15, 19, 30, 32, 33, 35, 37, 48, 50, 52, 57, 61, 65, 66, 85, 96, 100]
